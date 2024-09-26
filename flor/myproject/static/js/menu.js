01457fb64329fc489a8c9a203ffcdbf1b4971734
Yhozira <2022110530@udh.edu.pe>
2024-09-26 17:42:44 -0500

    Delete flor directory

--- flor/myproject/static/js/menu.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu');
-const menudesplegable = document.querySelector('.menu_desplegable')
-
-menu.addEventListener('click',()=>{
-    menu.classList.toggle("active");
-    menudesplegable.classList.toggle("click");
-})
